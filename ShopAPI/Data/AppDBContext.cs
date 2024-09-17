847cb703465140dab233568c0eed2761be6e0a56
apanchaldev <apanchal.dev@gmail.com>
2024-09-17 19:19:24 -0400

    extended Identityuser table with custom name column

--- ShopAPI/Data/AppDBContext.cs
+++ ShopAPI/Data/AppDBContext.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using ShopAPI.Models;
 
 namespace ShopAPI.Data
 {
-    public class AppDBContext : IdentityDbContext
+    public class AppDBContext : IdentityDbContext<ApplicationUser>
     {
         public AppDBContext(DbContextOptions options): base(options)
         {
-                
         }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
